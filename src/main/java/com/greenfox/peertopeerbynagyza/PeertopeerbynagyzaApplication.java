13e5bc3be8534cce7c4b8ca3ac13f4361b2608ea
Zsolt Nagy <nagyza@gmail.com>
2017-05-17 11:41:39 +0200

    restore project

---
+++ src/main/java/com/greenfox/peertopeerbynagyza/PeertopeerbynagyzaApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.peertopeerbynagyza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeertopeerbynagyzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeertopeerbynagyzaApplication.class, args);
+	}
+}
