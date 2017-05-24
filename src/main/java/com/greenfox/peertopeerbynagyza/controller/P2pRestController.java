2103e6339e469075d8673822f48cea0c9f774b49
Zsolt Nagy <nagyza@gmail.com>
2017-05-24 12:24:19 +0200

    Complete endpoint to send forward incoming message has foreign origin

--- src/main/java/com/greenfox/peertopeerbynagyza/controller/P2pRestController.java
+++ src/main/java/com/greenfox/peertopeerbynagyza/controller/P2pRestController.java
@@ -1,23 +1,35 @@
 package com.greenfox.peertopeerbynagyza.controller;
 
 import com.greenfox.peertopeerbynagyza.repository.MessageRepository;
+import com.greenfox.peertopeerbynagyza.repository.UsersRepository;
 import com.greenfox.peertopeerbynagyza.service.MessageWrapper;
 import com.greenfox.peertopeerbynagyza.service.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class P2pRestController {
 
+  private static final String CHAT_APP_UNIQUE_ID = "https://p2p-by-nagyza.herokuapp.com";
+
   @Autowired
   MessageRepository messageRepository;
 
-  @PostMapping("/api/message/receive")
+  @Autowired
+  UsersRepository usersRepository;
+
+  @PostMapping(value = "/api/message/receive", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   @CrossOrigin("*")
   public ResponseMessage receiveMessage(@RequestBody MessageWrapper messageWrapper) {
     ResponseMessage responseMessage = new  ResponseMessage("ok");
     messageRepository.save(messageWrapper.getMessage());
+    if (messageWrapper.getClient().getId() != usersRepository.findOne((long) 1).getUsername()) {
+      RestTemplate restTemplate = new RestTemplate();
+      restTemplate.postForObject(CHAT_APP_UNIQUE_ID + "/api/message/receive", messageWrapper, ResponseMessage.class);
+    }
     return responseMessage;
   }
 }
