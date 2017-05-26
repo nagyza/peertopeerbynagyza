16d00df873a9c4af136e9d981e48cbb20c5159ad
Zsolt Nagy <nagyza@gmail.com>
2017-05-26 14:38:14 +0200

    Refactor endpoint and response use of message validator service

--- src/main/java/com/greenfox/peertopeerbynagyza/controller/P2pRestController.java
+++ src/main/java/com/greenfox/peertopeerbynagyza/controller/P2pRestController.java
@@ -2,17 +2,15 @@ package com.greenfox.peertopeerbynagyza.controller;
 
 import com.greenfox.peertopeerbynagyza.repository.MessageRepository;
 import com.greenfox.peertopeerbynagyza.repository.UsersRepository;
-import com.greenfox.peertopeerbynagyza.service.MessageWrapper;
-import com.greenfox.peertopeerbynagyza.service.ResponseMessage;
+import com.greenfox.peertopeerbynagyza.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class P2pRestController {
 
-  private static final String CHAT_APP_UNIQUE_ID = System.getenv("CHAT_APP_UNIQUE_ID");
+  private static final String CHAT_APP_UNIQUE_ID = "https://p2p-by-nagyza.herokuapp.com/";
 
   @Autowired
   MessageRepository messageRepository;
@@ -20,16 +18,22 @@ public class P2pRestController {
   @Autowired
   UsersRepository usersRepository;
 
-  @PostMapping(value = "/api/message/receive", produces = MediaType.APPLICATION_JSON_VALUE)
+  @Autowired
+  MessageValidator messageValidator;
+
+  @PostMapping(value = "/api/message/receive")
   @ResponseBody
   @CrossOrigin("*")
   public ResponseMessage receiveMessage(@RequestBody MessageWrapper messageWrapper) {
-    ResponseMessage responseMessage = new  ResponseMessage("ok");
-    messageRepository.save(messageWrapper.getMessage());
-    if (!messageWrapper.getClient().getId().equals(usersRepository.findOne((long) 1).getUsername())) {
-      RestTemplate restTemplate = new RestTemplate();
-      restTemplate.postForObject(CHAT_APP_UNIQUE_ID + "/api/message/receive", messageWrapper, ResponseMessage.class);
+    if (messageValidator.isMessageWrapperValid(messageWrapper)) {
+      if (!messageWrapper.getClient().getId().equals(usersRepository.findOne((long) 1).getUsername())) {
+        messageRepository.save(messageWrapper.getMessage());
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(CHAT_APP_UNIQUE_ID + "/api/message/receive", messageWrapper, ResponseMessage.class);
+      }
+      return new ResponseMessage("ok");
+    } else {
+      return new ResponseErrorMessage("error", messageValidator.getMissingFieldsMessage(messageWrapper));
     }
-    return responseMessage;
   }
 }
